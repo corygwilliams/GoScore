3c4734df6029a75aa8b11520460be8dd597f91f2
Cory Williams <corygwilliams@gmail.com>
2011-04-12 22:42:59 -0400

    Fixing image size and darkening the overlay alpha

--- src/com/github/goscore/CameraOverlay.java
+++ src/com/github/goscore/CameraOverlay.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.View;
 
 public class CameraOverlay extends View {
@@ -25,7 +26,7 @@ public class CameraOverlay extends View {
     	}
     	Paint paint = new Paint();
     	paint.setColor(Color.GRAY);
-    	paint.setAlpha(128);
+    	paint.setAlpha(192);
     	paint.setStyle(Paint.Style.FILL);
     	canvas.drawRect(0, 0, boxWidth, height, paint);
     	canvas.drawRect(width - boxWidth, 0, width, height, paint);
@@ -34,7 +35,11 @@ public class CameraOverlay extends View {
     	float gridStart = gridStep * 2;
     	float gridEnd = gridStep * 38;
     	if (mBoardImage != null) {
-    		canvas.drawBitmap(mBoardImage, boxWidth + gridStart, gridStart, paint);
+    		Rect imageTarget = new Rect(
+    				(int)(boxWidth + gridStart), (int)gridStart,
+    				(int)(boxWidth + gridEnd), (int)gridEnd);
+    		Rect imageSource = new Rect(0, 0, mBoardImage.getWidth(), mBoardImage.getHeight());
+    		canvas.drawBitmap(mBoardImage, imageSource, imageTarget, paint);
     	} else {
 	    	paint.setColor(Color.RED);
 	    	for (int i = 1; i < 20; i++) {
